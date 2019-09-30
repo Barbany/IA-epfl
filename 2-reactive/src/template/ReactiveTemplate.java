68c8f643e4c415cf0e2d70959f06e26af129d5bf
Barbany <oriol.barbanymayor@epfl.ch>
2019-09-30 16:28:10 +0200

    It was not a suggestion...

---
+++ 2-reactive/src/template/ReactiveTemplate.java
@@ -0,0 +1,55 @@
+package template;
+
+import java.util.Random;
+
+import logist.simulation.Vehicle;
+import logist.agent.Agent;
+import logist.behavior.ReactiveBehavior;
+import logist.plan.Action;
+import logist.plan.Action.Move;
+import logist.plan.Action.Pickup;
+import logist.task.Task;
+import logist.task.TaskDistribution;
+import logist.topology.Topology;
+import logist.topology.Topology.City;
+
+public class ReactiveTemplate implements ReactiveBehavior {
+
+	private Random random;
+	private double pPickup;
+	private int numActions;
+	private Agent myAgent;
+
+	@Override
+	public void setup(Topology topology, TaskDistribution td, Agent agent) {
+
+		// Reads the discount factor from the agents.xml file.
+		// If the property is not present it defaults to 0.95
+		Double discount = agent.readProperty("discount-factor", Double.class,
+				0.95);
+
+		this.random = new Random();
+		this.pPickup = discount;
+		this.numActions = 0;
+		this.myAgent = agent;
+	}
+
+	@Override
+	public Action act(Vehicle vehicle, Task availableTask) {
+		Action action;
+
+		if (availableTask == null || random.nextDouble() > pPickup) {
+			City currentCity = vehicle.getCurrentCity();
+			action = new Move(currentCity.randomNeighbor(random));
+		} else {
+			action = new Pickup(availableTask);
+		}
+		
+		if (numActions >= 1) {
+			System.out.println("The total profit after "+numActions+" actions is "+myAgent.getTotalProfit()+" (average profit: "+(myAgent.getTotalProfit() / (double)numActions)+")");
+		}
+		numActions++;
+		
+		return action;
+	}
+}
