1ac9480880247e129cec4c1dbc896ded78454ad3
Barbany <oriol.barbanymayor@epfl.ch>
2019-10-03 17:19:42 +0200

    Working version of Q-Learning

--- 2-reactive/src/template/ReactiveTemplate.java
+++ 2-reactive/src/template/ReactiveTemplate.java
@@ -14,11 +14,18 @@ import logist.topology.Topology;
 import logist.topology.Topology.City;
 
 public class ReactiveTemplate implements ReactiveBehavior {
+	
+	private static double EPSILON = 1e-5;
 
 	private Random random;
 	private double discount;
 	private int numActions;
 	private Agent myAgent;
+	
+	// Policy: Random (0), Q-Learning (1)
+	// TODO: Note that policy that takes all the packets is also very good
+	private int policy = 1;
+	
 	private double[][][] Q;
 	private double[] V;
 	private double[] P_nopacket;
@@ -32,8 +39,10 @@ public class ReactiveTemplate implements ReactiveBehavior {
 				0.95);
 
 		this.random = new Random();
+		
+		// Discount used as probability of picking up when random policy
 		this.discount = discount;
-		this.numActions = 0;
+		this.numActions = 1;
 		this.myAgent = agent;
 		
 		// Run Offline Q-Learning
@@ -42,6 +51,11 @@ public class ReactiveTemplate implements ReactiveBehavior {
 	}
 	
 	public void reinforce(Topology topology, TaskDistribution td, Vehicle v) {
+		// Initialize V values to 0 (default in Java)
+		P_nopacket = new double[topology.cities().size()];
+		V = new double[topology.cities().size()];
+		Q = new double[topology.cities().size()][topology.cities().size()][2];
+		
 		// Compute probability of no having tasks at each city (so T(s,a,s') sums up to one for each s)
 		for (City city_a : topology) {
 			double p_nopacket = 0;
@@ -51,55 +65,79 @@ public class ReactiveTemplate implements ReactiveBehavior {
 			P_nopacket[city_a.id] = 1 - p_nopacket;
 		}
 		
-		// Initialize V values
-		// TODO: Check that this is a good initialization (o/w try with -max_distance * cost_km)
-		for (City city : topology) {
-			V[city.id] = 0;
-		}
 		
-		// TODO: Iterate UNTIL GOOD ENOUGH
-		for (int i = 0; i < 500; i++) {
+		double dif;
+		double old_Qval;
+		
+		// Iterate UNTIL GOOD ENOUGH
+		do {
+			dif = 0;
 			for (City city_a : topology) {				
 				for (City city_b : topology) {					
 					// Actions: Refuse (0) and Accept (1)
 					
 					if (city_a.hasNeighbor(city_b)) {
-						// Refuse only with neighbors
+						// Refusing a packet can only lead to neighbor states
+						old_Qval = Q[city_a.id][city_b.id][0];
 						Q[city_a.id][city_b.id][0] = - city_a.distanceTo(city_b) * v.costPerKm() +
 								discount * P_nopacket[city_a.id] / city_a.neighbors().size() * V[city_b.id];
 						
 						// Update V values if we find something better
-						if (Q[city_a.id][city_b.id][0] > V[city_a.id]) {
-							V[city_a.id] = Q[city_a.id][city_b.id][0];
-						}
+						V[city_a.id] = Math.max(Q[city_a.id][city_b.id][0], V[city_a.id]);
+						
+						// Check difference in Q value update
+						dif = Math.max(dif, Math.abs(Q[city_a.id][city_b.id][0] - old_Qval));
 					}
+					old_Qval = Q[city_a.id][city_b.id][1];
 					
-					// TODO: Check that the weight incurs a multiplicative cost by distance
 					Q[city_a.id][city_b.id][1] = td.reward(city_a, city_b) -
-							city_a.distanceTo(city_b) * v.costPerKm() * td.weight(city_a, city_b) +
+							city_a.distanceTo(city_b) * v.costPerKm() +
 							discount * td.probability(city_a, city_b) * V[city_b.id];
 					
-					if (Q[city_a.id][city_b.id][1] > V[city_a.id]) {
-						V[city_a.id] = Q[city_a.id][city_b.id][1];
-					}
+					// Update V values if we find something better
+					V[city_a.id] = Math.max(Q[city_a.id][city_b.id][1], V[city_a.id]);
+					
+					// Check difference in Q value update
+					dif = Math.max(dif, Math.abs(Q[city_a.id][city_b.id][1] - old_Qval));
 				}
 			}
-		}
+		} while (dif > EPSILON);
 	}
 
 	@Override
 	public Action act(Vehicle vehicle, Task availableTask) {
 		Action action;
 
-		/* Random policy
-		if (availableTask == null || random.nextDouble() > pPickup) {
-			City currentCity = vehicle.getCurrentCity();
-			action = new Move(currentCity.randomNeighbor(random));
-		} else {
-			action = new Pickup(availableTask);
+		//Random policy
+		switch(policy) {
+		case 0:
+			// Random policy
+			if (availableTask == null || random.nextDouble() > discount) {
+				City currentCity = vehicle.getCurrentCity();
+				action = new Move(currentCity.randomNeighbor(random));
+			} else {
+				action = new Pickup(availableTask);
+			}
+			break;
+		case 1:
+		default:
+			// Off-line Q-Learning: Greedy policy
+			City city_a = vehicle.getCurrentCity();
+			
+			double max_qval = 0;
+			City max_neigh = city_a;
+			
+			for (City city_b: city_a.neighbors()) {
+				max_qval = Math.max(max_qval, Q[city_a.id][city_b.id][0]);
+				max_neigh = city_b;
+			}
+			if (availableTask != null && (max_qval < Q[city_a.id][availableTask.deliveryCity.id][1])) {
+				action = new Pickup(availableTask);
+			} else {
+				action = new Move(max_neigh);
+			}
+			break;
 		}
-		*/
-		action = null;
 		
 		if (numActions >= 1) {
 			System.out.println("The total profit after "+numActions+" actions is "+myAgent.getTotalProfit()+" (average profit: "+(myAgent.getTotalProfit() / (double)numActions)+")");
