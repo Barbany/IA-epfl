74fe52d1fab9c1db3c19bb900a20423ec596b234
Barbany <oriol.barbanymayor@epfl.ch>
2019-11-21 21:17:08 +0100

    Minor modification

--- 5-auction/run_tournament.sh
+++ 5-auction/run_tournament.sh
@@ -3,4 +3,4 @@ echo "Starting tournament"
 java -jar ../logist/logist.jar -new myTournament agents
 java -jar ../logist/logist.jar -run myTournament config/auction.xml
 java -jar ../logist/logist.jar -score myTournament
-less tournament/myTournament/results.txt
+cat tournament/myTournament/results.txt
