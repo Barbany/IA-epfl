634db9b0470e418b031270a342a14819001f762a
Barbany <oriol.barbanymayor@epfl.ch>
2019-11-23 18:31:34 +0100

    Update gitignore

--- 5-auction/run_tournament.sh
+++
@@ -1,6 +0,0 @@
-rm -rf tournament/myTournament
-echo "Starting tournament"
-java -jar ../logist/logist.jar -new myTournament agents
-java -jar ../logist/logist.jar -run myTournament config/auction.xml
-java -jar ../logist/logist.jar -score myTournament
-cat tournament/myTournament/results.txt
