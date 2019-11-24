7d40c0d9ea9f7aa967691eb4ab392c35021cb278
Barbany <oriol.barbanymayor@epfl.ch>
2019-11-24 11:33:31 +0100

    Easy tournaments

---
+++ 5-auction-tournament/run_tournament.sh
@@ -0,0 +1,16 @@
+rm -rf tournament/myTournament
+rm -rf agents
+
+mkdir agents
+
+echo "Write space separated names of agents that will be enrolled in the tournament"
+ls all_agents
+read files
+for file in $files; do
+        mv all_agents/$file.jar agents/$file.jar
+done        
+echo "Starting tournament"
+java -jar ../logist/logist.jar -new myTournament agents
+java -jar ../logist/logist.jar -run myTournament config/auction.xml
+java -jar ../logist/logist.jar -score myTournament
+cat tournament/myTournament/results.txt
